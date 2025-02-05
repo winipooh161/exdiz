85a250700b024b10962c42ff85afd204cae39f36
winipoh161 <w1nishko@yandex.ru>
2025-02-05 16:49:14 +0300

    2223

---
+++ storage/framework/views/78a0be18e5fbb38ad08f644575c5e8b9.php
@@ -0,0 +1,126 @@
+
+<h1 class="flex">
+    Детали  <span class="Jikharev">брифа </span> 
+</h1>
+<table class="table table-bordered" style="">
+    <thead>
+        <tr style="">
+            <th style="">Поле</th>
+            <th style="">ОТВЕТ</th>
+        </tr>
+    </thead>
+    <tbody style="margin-bottom: 30px;">
+        <tr>
+            <td style="">Артикль</td>
+            <td style=""><?php echo e($brif->article); ?></td>
+        </tr>
+        <tr>
+            <td style="">Название</td>
+            <td style=""><?php echo e($brif->title); ?></td>
+        </tr>
+        <tr>
+            <td style="">Общая сумма</td>
+            <td style=""><?php echo e($brif->price); ?> руб</td>
+        </tr>
+        <tr>
+            <td style="">Описание</td>
+            <td style=""><?php echo e($brif->description); ?></td>
+        </tr>
+        <tr>
+            <td style="">Статус</td>
+            <td style=""><?php echo e($brif->status); ?></td>
+        </tr>
+        <tr>
+            <td style="">Создатель брифа</td>
+            <td style=""><?php echo e($user->name); ?></td>
+        </tr>
+        <tr>
+            <td style="">Номер клиента</td>
+            <td style=""><?php echo e($user->phone); ?></td>
+        </tr>
+    </tbody>
+    <tbody>
+        
+        <?php for($i = 1; $i <= 15; $i++): ?>
+            <tr>
+                <td colspan="2" style=" ">
+                    <h3 style="font-size: 16px; margin: 0; text-align:left;"><?php echo e($pageTitlesCommon[$i - 1]); ?></h3>
+                </td>
+            </tr>
+            <?php $__currentLoopData = $questions[$i]; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $question): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                <?php
+                    $field = $question['key'];
+                ?>
+                <?php if(isset($brif->$field)): ?>
+                    <tr>
+                        <td style=""><?php echo e($question['title']); ?></td>
+                        <td style=""><?php echo e($brif->$field); ?></td>
+                    </tr>
+                <?php endif; ?>
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+        <?php endfor; ?>
+    </tbody>
+
+    <table class="table table-bordered" style="">
+        <thead>
+            <tr style="">
+                <th style="">Название файла</th>
+                <th style="">Действие</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php if($brif->documents && is_array(json_decode($brif->documents, true))): ?>
+                <?php $__empty_1 = true; $__currentLoopData = json_decode($brif->documents, true); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $document): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); $__empty_1 = false; ?>
+                    <tr>
+                        <td style=""><?php echo e(basename($document)); ?></td>
+                        <td style="">
+                            <a href="<?php echo e(asset($document)); ?>" target="_blank">Скачать</a>
+                        </td>
+                    </tr>
+                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); if ($__empty_1): ?>
+                    <tr>
+                        <td colspan="2" style=" text-align: center;">Документов не найдено</td>
+                    </tr>
+                <?php endif; ?>
+            <?php else: ?>
+                <tr>
+                    <td colspan="2" style=" text-align: center;">Документов не найдено</td>
+                </tr>
+            <?php endif; ?>
+        </tbody>
+        
+    </table>
+    <tbody>
+        <tr>
+            <td style="">Дата создания</td>
+            <td style=""><?php echo e($brif->created_at); ?></td>
+        </tr>
+        <tr>
+            <td style="">Дата обновления</td>
+            <td style=""><?php echo e($brif->updated_at); ?></td>
+        </tr>
+    </tbody>
+</table>
+
+<style>
+   
+
+    table {
+        border: 1px solid #ddd;
+      
+        width: 100%;
+    }
+
+    th, td {
+      
+        border: 1px solid #ddd;
+    }
+
+    th {
+       
+        font-weight: bold;
+    }
+
+ 
+</style>
+<?php /**PATH C:\OSPanel\domains\dlk\resources\views\common\module\show.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/a8f7c9b162d9e241e5c7ca38a0b2a933.php
@@ -0,0 +1,49 @@
+<?php $__env->startSection('content'); ?>
+<div class="container-auth">
+    <div class="auth__body flex center">
+        <div class="auth__form">
+            <h1>Вход</h1>
+            <p class="auth__title_sub">Мы рады видеть вас! </br>
+                Войдите в свою учетную запись</p>
+            <form action="<?php echo e(route('login.password.post')); ?>" method="POST">
+                <?php echo csrf_field(); ?>
+                <label for="phone">
+                    <p>Телефон:</p>
+                    <input type="phone" name="phone" id="phone" class="form-control maskphone"placeholder="+7 (___) ___-__-__"  value="<?php echo e(old('phone')); ?>"  maxlength="50" required>
+                    <?php $__errorArgs = ['phone'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                        <div class="alert alert-danger"><?php echo e($message); ?></div>
+                    <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                </label>
+                <label for="password">
+                    <p>Пароль:</p>
+                    <input type="password" name="password" id="password" placeholder="********"   maxlength="50" class="form-control" required>
+                    <?php $__errorArgs = ['password'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                        <div class="alert alert-danger"><?php echo e($message); ?></div>
+                    <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                </label>
+                <button type="submit" class="btn btn-primary">Войти</button>
+            </form>
+            <ul class="auth__form__link">
+                <li><a href="<?php echo e(route('login.code')); ?>">Войти по коду</a></li>
+                <li><a href="<?php echo e(route('register')); ?>">Регистрация</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.auth', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\OSPanel\domains\dlk\resources\views\auth\login-password.blade.php ENDPATH**/ ?>
